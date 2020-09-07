bed7f604bba2d7c3c73486e171483cccabacfbb7
TanyaStk <64659217+TanyaStk@users.noreply.github.com>
2020-09-07 23:23:39 +0300

    Initial commit

---
+++ src/com/uni/TailorSeriesCalculator.java
@@ -0,0 +1,45 @@
+package com.uni;
+import java.util.Scanner;
+
+public class TailorSeriesCalculator {
+    public static void main(String[] args) {
+	    Scanner in = new Scanner(System.in);
+        System.out.println("Enter k: ");
+        int k = in.nextInt();
+        double e = Math.pow(10, -k);
+        System.out.println("Enter x (-1, 1): ");
+        double x = in.nextDouble();
+        try {
+            if (x <= -1 | x >= 1) {
+                throw new Exception("x is out of range");
+            }
+                double counted_ans = countValue(x, e);
+                double func_ans = Math.asin(x);
+                System.out.printf("Counted result: %.3f \n", counted_ans);
+                System.out.printf("Function result: %.3f \n", func_ans);
+        }
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        in.close();
+    }
+    static double countValue(double x, double e){
+        double elem = 100;
+        double result = 0;
+        int n = 0;
+        while (Math.abs(elem) >= Math.abs(e)){
+            elem = (getFactorial(2 * n) * Math.pow(x, 2 * n + 1))/
+                    (Math.pow(4, n) * Math.pow(getFactorial(n), 2) * (2 * n + 1));
+            result += elem;
+            n++;
+        }
+        return result;
+    }
+    static int getFactorial(int n) {
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * getFactorial(n - 1);
+        }
+    }
+}
---
+++ src/com/uni/TailorSeriesCalculatorTest.java
@@ -0,0 +1,47 @@
+package com.uni;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TailorSeriesCalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void test1CountValue() {
+        double x = 0;
+        double eps = Math.pow(10, -10);
+        double expected = Math.asin(x);
+        double actual = TailorSeriesCalculator.countValue(x, eps);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void test2CountValue() {
+        double x = 0.5;
+        double eps = Math.pow(10, -10);
+        double expected = Math.asin(x);
+        double actual = TailorSeriesCalculator.countValue(x, eps);
+        assertNotEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void test3CountValue() {
+        double x = -0.5;
+        double eps = Math.pow(10, -10);
+        double expected = Math.asin(x);
+        double actual = TailorSeriesCalculator.countValue(x, eps);
+        assertNotEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void test1GetFactorial() {
+        int n = 5;
+        int actual = TailorSeriesCalculator.getFactorial(n);
+        assertEquals(120, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void test2GetFactorial() {
+        int n = 1;
+        int actual = TailorSeriesCalculator.getFactorial(n);
+        assertEquals(1, actual);
+    }
+}
